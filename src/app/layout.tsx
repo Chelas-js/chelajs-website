243a85f5d7c94ec70fc87dccc18f1a72b392b46f
Jonathan Delgado <hi@jon.soy>
2025-08-15 10:12:06 -0400

    refactor: remove Footer component from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <div className="flex-grow">{children}</div>
-        <Footer />
       </body>
     </html>
   );
